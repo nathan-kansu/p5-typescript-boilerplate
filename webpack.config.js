399f7bbfce622bed3aabccd88b3c88c37d5bdeae
Nathan Tredgett <nathantredgett@gmail.com>
2019-03-07 10:35:50 +0000

    added typescript support to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ],
     rules: [
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: ["file-loader"]
       }
     ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
   },
   devtool: "inline-source-map",
   devServer: {
@@ -23,7 +33,7 @@ module.exports = {
     })
   ],
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   }
 };
