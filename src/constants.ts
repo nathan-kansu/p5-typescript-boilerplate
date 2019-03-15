35ec9e76d596ca985fd91a1b383f62b24049dbe1
Nathan Tredgett <nathantredgett@gmail.com>
2019-03-15 17:10:11 +0000

    added constants file

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const CANVAS_HEIGHT = 600;
+export const CANVAS_WIDTH = 600;
+export const COLOR_BACKGROUND = [51, 51, 51];
+export const FRAME_RATE = 60;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import * as p5 from "p5";
+import {
+  CANVAS_HEIGHT,
+  CANVAS_WIDTH,
+  COLOR_BACKGROUND,
+  FRAME_RATE
+} from "./constants";
+
 import "./style.css";
 
 const sketch = (p5: p5) => {
-  p5.setup = () => {};
+  p5.setup = () => {
+    p5.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    p5.background(COLOR_BACKGROUND);
+    p5.frameRate(FRAME_RATE);
+  };
 
   p5.draw = () => {
     p5.ellipse(50, 50, 80, 80);
