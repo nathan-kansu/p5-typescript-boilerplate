fa25e407370e0d6fd159b59ab7c2cda4340165fc
Nathan Tredgett <nathantredgett@gmail.com>
2019-03-21 16:35:52 +0000

    added functionality to export animation frames using "CCapture"

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const CANVAS_HEIGHT = 600;
+export const CANVAS_ID = "defaultCanvas0";
 export const CANVAS_WIDTH = 600;
 export const COLOR_BACKGROUND = [51, 51, 51];
 export const FRAME_RATE = 60;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as p5 from "p5";
 import {
   CANVAS_HEIGHT,
+  CANVAS_ID,
   CANVAS_WIDTH,
   COLOR_BACKGROUND,
   FRAME_RATE
@@ -9,14 +10,37 @@ import {
 import "./style.css";
 
 const sketch = (p5: p5) => {
+  const capturer = new CCapture({
+    format: "jpg",
+    framerate: FRAME_RATE,
+    quality: 100
+  });
+  let canvas: HTMLElement | null;
+  let exportAnimation: Boolean = false;
+
   p5.setup = () => {
     p5.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
     p5.background(COLOR_BACKGROUND);
     p5.frameRate(FRAME_RATE);
+    canvas = document.getElementById(CANVAS_ID);
+
+    if (exportAnimation) {
+      capturer.start();
+    }
   };
 
   p5.draw = () => {
     p5.ellipse(50, 50, 80, 80);
+
+    if (exportAnimation) {
+      if (canvas) {
+        capturer.capture(canvas);
+      }
+
+      p5.noLoop();
+      capturer.stop();
+      capturer.save();
+    }
   };
 };
 
