fcffec103e2b127eb5ec8404a990b503c4f32ab7
Nathan Tredgett <nathantredgett@gmail.com>
2019-04-24 21:01:21 +0100

    updated default constant values to be instagram friendly

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const CANVAS_HEIGHT = 600;
+export const CANVAS_HEIGHT = 1080;
 export const CANVAS_ID = "defaultCanvas0";
-export const CANVAS_WIDTH = 600;
+export const CANVAS_WIDTH = 1080;
 export const COLOR_BACKGROUND = [51, 51, 51];
-export const FRAME_RATE = 60;
+export const FRAME_RATE = 30;
